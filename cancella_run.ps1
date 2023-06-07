5996d5ae6f28c5a4a6e7792838f424bd19ca1c77
simonemichele05 <135108223+simonemichele05@users.noreply.github.com>
2023-06-07 14:42:44 +0200

    Delete cancella_run.ps1

--- cancella_run.ps1
+++
@@ -1,5 +0,0 @@
-$regKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
-
-if (Test-Path $regKey) {
-    Remove-ItemProperty -Path $regKey -Name '*' -ErrorAction SilentlyContinue
-}
