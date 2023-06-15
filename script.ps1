9be9621f70779518a7f602d13701a779246e7b54
simonemichele05 <135108223+simonemichele05@users.noreply.github.com>
2023-06-15 17:52:02 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -352,7 +352,7 @@ while ($n -clt $config.NumberScreenshot -or $config.NumberScreenshot -eq 0) {
 		}
 
 		# controllo quante volte deve essere eseguito ancora il comando
-		if($command -ne "" -and $command[1] -ne 0 -and $command.Count -gt 0){
+		if($command -ne "" -and $command[1] -ne 0 -and $command.Count -gt 1){
 			if($repeatCode -cgt 1){
 				$repeatCode -= 1
 			}else{
